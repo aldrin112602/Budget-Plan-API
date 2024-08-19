99401a9eb238f1b6b86ccd751019a78a1f4aecc7
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-08-19 11:08:24 +0800

    Push Changes

---
+++ README.md
@@ -0,0 +1,9 @@
+### Generate the Prisma Client
+```bash
+npx prisma generate
+```
+
+### Push Schema to MongoDB
+```bash
+npx prisma db push
+```
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import authRoutes from './routes/authRoutes';
+import userRoutes from './routes/userRoutes';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
+export default app;
