ef989d2c9437ab25480f94a06cf3e0283bf38fa7
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-08-22 19:22:53 +0800

    Push Changes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes';
-import userRoutes from './routes/userRoutes';
-
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
 
@@ -18,7 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // http://localhost:8000/api/auth/register  [POST]
 // http://localhost:8000/api/user/profile   [POST, GET]
 
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 export default app;
